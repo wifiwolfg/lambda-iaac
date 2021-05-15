ac1dcdbb6d2fda9f07f68e3a97b62284921dfae0
wifiwolf <75647464+wifiwolfg@users.noreply.github.com>
2021-05-15 12:24:57 +0900

    updated python code

--- lambda-params.py
+++ lambda-params.py
@@ -4,35 +4,22 @@ import os
 from datetime import datetime
 from urllib.parse import urlparse
 
-params_url = "s3://parameterscloudformation/demo-params.json"
-template_url = "https://parameterscloudformation.s3.amazonaws.com/cloudformationtest.json"
-
-def parse_params():
-  s3 = boto3.resource('s3')
-  s3_parse = urlparse(params_url)
-  bucket = s3_parse.netloc
-  s3_key = s3_parse.path.lstrip('/')
-  s3_obj = s3.Object(bucket, s3_key)
-  template_raw_data = s3_obj.get()["Body"].read().decode('utf-8')
-  template_params = json.loads(template_raw_data)
-  return template_params
+template_url = "https://yourlink.s3.amazonaws.com/s3template.json"
 
 def launch_stack():
   cfn = boto3.client('cloudformation')
   current_ts = datetime.now().isoformat().split('.')[0].replace(':','-')
-  stackname = 'stack-test-from-lambda' + current_ts
+  stackname = 'stack-test-from-lambda-s3' + current_ts
   capabilities = ['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND']
   try:
-    template_params = parse_params()
     stackdata = cfn.create_stack(
       StackName=stackname,
       DisableRollback=True,
-      TemplateURL=template_url,
-      Parameters=template_params,
+      TemplateURL=template_url,ud
       Capabilities=capabilities)
   except Exception as e:
     print(str(e))
-  return stackdata
+  return stackdata  
 
 def lambda_handler(event, context):
   print("Received event:")
